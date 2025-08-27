16fd75964d593774f0b776afbb07bc55c717fc60
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-08-27 19:12:41 +0330

    Added: constructor functions for various types of things

--- internal/thing/thing.go
+++ internal/thing/thing.go
@@ -10,13 +10,14 @@ import (
 type Tag int
 
 const (
-	TagTArray  Tag = iota
-	TagTList   Tag
-	TagTProc   Tag
-	TagTEnv    Tag
-	TagTString Tag
-	TagTSymbol Tag
-	TagTNumber Tag
+	TagTArray    Tag = iota
+	TagTList     Tag
+	TagTProc     Tag
+	TagTPropList Tag
+	TagTString   Tag
+	TagTSymbol   Tag
+	TagTNumber   Tag
+	TagTParam    Tag
 )
 
 type TArray struct {
@@ -26,18 +27,22 @@ type TArray struct {
 	origin uint
 }
 
-type TList struct {
-	values []*Thing
-	size   uint
-}
-
 type TProc struct {
-	env     TEnv
-	tree    ast.ASTNode
-	nparams int
+	env    map[TSymbol]*Thing
+	tree   ast.ASTNode
+	defn   string
+	params []TParam
 }
 
-type TEnv map[TSymbol]*Thing
+type TParam struct {
+	name   TSymbol
+	dflval *Thing
+	opt    bool
+	rem    bool
+}
+
+type TList []*Thing
+type TPropList map[*Thing]*Thing
 
 type TString string
 type TNumber float64
@@ -46,4 +51,52 @@ type TSymbol string
 type Thing struct {
 	value interface{}
 	tag   Tag
+	local bool
+}
+
+func New(value interface{}, tag Tag, local bool) *Thing {
+	tng := &Thing{
+		value: value,
+		tag:   tag,
+		local: local,
+	}
+	return tng
+}
+
+func NewArray(values []*Thing, size, dims, origin uint, local bool) *Thing {
+	arr := TArray{
+		values: values,
+		size:   size,
+		dims:   dims,
+		origin: origin,
+	}
+	return New(arr, TagTArray, local)
+}
+
+func NewList(values TList, local bool) *Thing {
+	return New(value, TagTList, local)
+}
+
+func NewPropList(values TPropList, local bool) *Thing {
+	return New(values, TagTPropList, local)
+}
+
+func NewProc(env map[TSymbol]*Thing, tree ast.Ast, defn string, params []TParam) *Thing {
+	proc := TProc{
+		env:    env,
+		tree:   tree,
+		defn:   defn,
+		params: params,
+	}
+	return New(proc, TagTProc, false)
+}
+
+func NewPram(name TSymbol, dflval *Thing, opt, rem bool) *Thing {
+	param := TParam{
+		name:   name,
+		dflval: dflval,
+		opt:    opt,
+		rem:    rem,
+	}
+	return New(param, TagTParam, true)
 }
